706239497cad02e742122ffdd95758bb83c58fac
Scove <Trannguyenp303@gmail.com>
2025-06-10 10:41:25 +0700

    Initial commit

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Day1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(sum());
+        Console.WriteLine(sub());
+        Console.WriteLine(product());
+        Console.WriteLine(divide());
+       
+    }
+
+
+    static double sum()
+    {
+        double p1, p2;
+        Console.WriteLine("Enter first number: ");
+        p1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter second number: ");
+        p2 = double.Parse(Console.ReadLine());
+        return p1 + p2;
+    }
+
+    static double sub()
+    {
+        double p1, p2;
+        Console.WriteLine("Enter first number: ");
+        p1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter second number: ");
+        p2 = double.Parse(Console.ReadLine());
+        return p1 - p2;
+    }
+
+    static double product()
+    {
+        double p1, p2;
+        Console.WriteLine("Enter first number: ");
+        p1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter second number: ");
+        p2 = double.Parse(Console.ReadLine());
+        return p1 * p2;
+    }
+
+    static double divide()
+    {
+        double p1, p2;
+        Console.WriteLine("Enter first number: ");
+        p1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter second number: ");
+        p2 = double.Parse(Console.ReadLine());
+        try
+        {
+            return p1 / p2;
+        }
+        catch (DivideByZeroException)
+        {
+            return 0;
+        }
+    }
+}
