d7832823437752fdccaa6b53f176301c6e498e1d
Scove <Trannguyenp303@gmail.com>
2025-06-11 11:01:42 +0700

    Day 2 exercise

--- LAB/Lab02/Lab02/Lab02/Program.cs
+++ LAB/Lab02/Lab02/Lab02/Program.cs
@@ -6,44 +6,72 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        double a = int.Parse(Console.ReadLine());
-        double b = int.Parse(Console.ReadLine());
-        Console.WriteLine("Thuong la: " + divide(a, b));
-    }
-
-    static double divide(double a, double b)
-    {
+        int x = 0 , y = 0;
+        string filePath = "input.txt";
+        StreamWriter writer = null;
         try
         {
-            return a / b;
-        }
-        catch (DivideByZeroException e)
-        {
-            Console.WriteLine(e.Message);
-            Console.WriteLine(e.Source);
-            Console.WriteLine(e.StackTrace);
-            Console.WriteLine(e.InnerException);
-            throw new DivideByZeroException("Division by zero", e);
+            Console.WriteLine("Enter the first number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            writer = new StreamWriter(filePath);
+
+            try
+            {
+                int result = Calculate(x, y);
+                writer.WriteLine("Calculation result: " + result);
+                Console.WriteLine("Result written to file.");
+            }
+            catch (NotNegativeException e)
+            {
+                writer.WriteLine("Calculation error: " + e.Message);
+                Console.WriteLine("Result written to file.");
+            }
+            catch (DivideByZeroException e)
+            {
+                writer.WriteLine("Division error: " + e.Message);
+                Console.WriteLine("Result written to file.");
+            }
         }
         catch (FormatException e)
         {
             Console.WriteLine(e.Message);
-            throw new FormatException("Format", e);
         }
-        catch (ArithmeticException e)
+        catch (IOException e)
         {
-            Console.WriteLine(e.Message);
-            throw new ArithmeticException("Arithmetic", e);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-            throw new Exception("Unknown Exception");
+            Console.WriteLine("File error: " + e.Message);
         }
         finally
         {
-            Console.WriteLine("finally:");
+            if (writer != null)
+            {
+                writer.Close();
+                Console.WriteLine("Closing the file.");
+            }
         }
+
+    }
+
+    static int Calculate(int x, int y)
+    {
+        if ((Math.Sqrt(3 * x + 2 * y)) <= 0)
+        {
+            throw new NotNegativeException("Square root of 3 x 2 x 3 is negative");
+        }
+
+        if ((2 * x - y) == 0)
+        {
+            throw new DivideByZeroException("Divide by zero is invalid");
+        }
+        
+        return (int)(Math.Sqrt(3 * x + 2 * y)) / (2 * x - y);
+        
+    }
+
+    public class NotNegativeException : Exception
+    {
+        public NotNegativeException(string message) : base(message) { }
     }
 }
