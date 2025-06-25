d7ee11dfffb1ef2837463cbdd72edce908acb05a
Scove <Trannguyenp303@gmail.com>
2025-06-25 11:17:36 +0700

    Semi Final test

---
+++ SEMI_FINAL/SEMI_FINAL/Player.cs
@@ -0,0 +1,30 @@
+﻿namespace SEMI_FINAL;
+
+public class Player
+{
+    public string PlayerID { get; set; }
+    public string Name { get; set; }
+    public int Gold { get; set; }
+    public int Score { get; set; }
+
+    public Player()
+    {
+        this.PlayerID = "";
+        this.Name = "";
+        this.Gold = 0;
+        this.Score = 0;
+    }
+
+    public Player(string playerID, string name, int gold, int score)
+    {
+        this.PlayerID = playerID;
+        this.Name = name;
+        this.Gold = gold;
+        this.Score = score;
+    }
+
+    public override string ToString()
+    {
+        return $"PLayer ID: {PlayerID}, Name: {Name}, Gold: {Gold}, Score: {Score}";
+    }
+}
